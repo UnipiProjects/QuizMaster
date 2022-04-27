9fac765d73800b1bbcaa9ffab053dae27bb88096
galiatsatouaikaterini <galiatsatouk@gmail.com>
2022-04-27 21:34:07 +0300

    player controller

---
+++ QuizMaster/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizMaster.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult StartQuiz()
+        {
+            return View();
+        }
+        public IActionResult ViewRank()
+        {
+            return View();
+        }
+        public IActionResult UpgradePremium()
+        {
+            return View();
+        }
+    }
+}
