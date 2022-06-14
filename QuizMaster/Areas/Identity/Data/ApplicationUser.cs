cb1de705e803576578d029d28fe7519f249f4bfe
Αικατερίνη Γαλιατσάτου <galiatsatouk@gmail.com>
2022-06-14 16:04:00 +0300

    Reset of Question daily

--- QuizMaster/Areas/Identity/Data/ApplicationUser.cs
+++ QuizMaster/Areas/Identity/Data/ApplicationUser.cs
@@ -11,11 +11,12 @@ namespace QuizMaster.Areas.Identity.Data
     public class ApplicationUser : IdentityUser
     {
         [PersonalData]
-        [Column(TypeName ="nvarchar(100)")]        
+        [Column(TypeName = "nvarchar(100)")]
         public string FirstName { get; set; }
         [PersonalData]
         [Column(TypeName = "nvarchar(100)")]
-        public string LastName { get; set; }                        
+        public string LastName { get; set; }
+        public DateTimeOffset LastLogin{get;set;}
 
     }
 }
